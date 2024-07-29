8ce0b419fe6c2ef9431f0ea43d876ea797860b2e
Aozin <59066161+Aozin@users.noreply.github.com>
2024-07-29 09:08:47 +0100

    implement post hook

---
+++ src/hooks/usePost.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+
+export default function usePost<T>(url: string, inData:T) {
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        fetch(url, {method: "POST", mode:"cors", body: JSON.stringify(inData)})
+            .then(async res => { 
+                if(!res.ok) throw await res.json();
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+            })
+            .catch(error => {
+                console.error("Error fetching data, ",  error)
+                setError(error)
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+
+    }, [])
+
+    return {data, loading, error}
+}
